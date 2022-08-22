83f341058dec417200bf2d07dbf4817394bcb85f
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-08-22 15:00:51 +0200

    changed classNames on Counter & render Counter inside Layaout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Layout from "./components/Layout/Layout";
-import Counter from "./components/Counter/Counter";
 
 const App = (): JSX.Element => {
   return (
     <>
       <Layout />
-      <Counter />
     </>
   );
 };
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,21 +1,21 @@
 const Counter = (): JSX.Element => {
   return (
     <ul className="counter-container">
-      <li className="counter-days">
-        <span className="counter-days__numbers">02</span>
-        <span className="counter-days__title ">days</span>
+      <li className="counter-item">
+        <span className="counter-item__numbers">02</span>
+        <span className="counter-item__title ">days</span>
       </li>
-      <li className="counter-hours">
-        <span className="counter-hours__numbers">20</span>
-        <span className="counter-hours__title ">hours</span>
+      <li className="counter-item">
+        <span className="counter-item__numbers">20</span>
+        <span className="counter-item__title ">hours</span>
       </li>
-      <li className="counter-minutes">
-        <span className="counter-minutes__numbers">34</span>
-        <span className="counter-minutes__title ">minutes</span>
+      <li className="counter-item">
+        <span className="counter-item__numbers">34</span>
+        <span className="counter-item__title ">minutes</span>
       </li>
-      <li className="counter-seconds">
-        <span className="counter-seconds__numbers">09</span>
-        <span className="counter-seconds__title ">seconds</span>
+      <li className="counter-item">
+        <span className="counter-item__numbers">09</span>
+        <span className="counter-item__title ">seconds</span>
       </li>
     </ul>
   );
