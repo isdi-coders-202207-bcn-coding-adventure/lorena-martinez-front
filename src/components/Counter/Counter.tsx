5fa1f4c69669176189ff5f5637707465f88a17f2
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-08-23 16:42:29 +0200

    counter functionality

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,22 +1,56 @@
+import { useEffect, useState } from "react";
+import deadLine from "../../utils";
 import StyledCounter from "./StyledCounter";
 
 const Counter = (): JSX.Element => {
+  interface Timer {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
+
+  const initalTimer: Timer = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  const [timeLeft, setTimeLeft] = useState(initalTimer);
+
+  useEffect(() => {
+    setInterval(() => {
+      const timeLeftToBeFree = deadLine - new Date().getTime();
+      setTimeLeft({
+        days: Math.floor(timeLeftToBeFree / (1000 * 60 * 60 * 24)),
+        hours: Math.floor(
+          (timeLeftToBeFree % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ),
+        minutes: Math.floor(
+          (timeLeftToBeFree % (1000 * 60 * 60)) / (1000 * 60)
+        ),
+        seconds: Math.floor((timeLeftToBeFree % (1000 * 60)) / 1000),
+      });
+    }, 1000);
+  }, []);
+
   return (
     <StyledCounter className="counter-container">
       <li className="counter-item">
-        <span className="counter-item__numbers">02</span>
+        <span className="counter-item__numbers">{timeLeft.days}</span>
         <span className="counter-item__text ">days</span>
       </li>
       <li className="counter-item">
-        <span className="counter-item__numbers">20</span>
+        <span className="counter-item__numbers">{timeLeft.hours}</span>
         <span className="counter-item__text">hours</span>
       </li>
       <li className="counter-item">
-        <span className="counter-item__numbers">34</span>
+        <span className="counter-item__numbers">{timeLeft.minutes}</span>
         <span className="counter-item__text">minutes</span>
       </li>
       <li className="counter-item">
-        <span className="counter-item__numbers">09</span>
+        <span className="counter-item__numbers">{timeLeft.seconds}</span>
         <span className="counter-item__text">seconds</span>
       </li>
     </StyledCounter>
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+const deadLine = new Date("September 16, 2022 00:00:00").getTime();
+
+export default deadLine;
